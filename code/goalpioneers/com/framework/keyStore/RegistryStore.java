ede7415edecffde5739cee1864072b079f4c45eb
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-07-13 16:39:55 +0200

    added code header

--- code/goalpioneers/com/framework/keyStore/RegistryStore.java
+++ code/goalpioneers/com/framework/keyStore/RegistryStore.java
@@ -1,8 +1,20 @@
+/**
+ * Copyright 2022, Goal Pioneers
+ * License: MIT License (https://github.com/GoalPioneers/Prototype.KeyStore/blob/main/LICENSE.md)
+ * Author: Kent v. Madsen
+ * Contact: Kent.vejrup.madsen@outlook.com
+ * Issues: https://github.com/GoalPioneers/Prototype.KeyStore/issues
+ * Title: 
+ * Description: 
+ * Tags: 
+ */
 package goalpioneers.com.framework.keyStore;
 
+//
 import java.util.ArrayList;
 import java.util.List;
 
+// 
 import goalpioneers.com.framework.keyStore.framework.KeyStoreFramework;
 import goalpioneers.com.framework.keyStore.framework.templates.KeyStoreFacade;
 
@@ -16,7 +28,7 @@ public class RegistryStore
     implements Store
 {
     // Constructors
-    /**
+	/**
      * 
      */
     public RegistryStore()
--- code/goalpioneers/com/framework/keyStore/framework/hashes/files/KeyStoreHashFilesBaseTemplate.java
+++ code/goalpioneers/com/framework/keyStore/framework/hashes/files/KeyStoreHashFilesBaseTemplate.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright 2022, Goal Pioneers
+ * License: MIT License (https://github.com/GoalPioneers/Prototype.KeyStore/blob/main/LICENSE.md)
+ * Author: Kent v. Madsen
+ * Contact: Kent.vejrup.madsen@outlook.com
+ * Issues: https://github.com/GoalPioneers/Prototype.KeyStore/issues
+ * Title: 
+ * Description: 
+ * Tags: 
+ */
 package goalpioneers.com.framework.keyStore.framework.hashes.files;
 
 //
