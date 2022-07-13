8ecc42c90ca54680056af2dd83976bd875952412
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-07-13 16:02:57 +0200

    work

--- code/goalpioneers/com/framework/keyStore/debugging/TestingKeyStoreElement.java
+++ code/goalpioneers/com/framework/keyStore/debugging/TestingKeyStoreElement.java
@@ -4,7 +4,8 @@ import goalpioneers.com.framework.keyStore.framework.templates.KeyStoreElement;
 
 
 /**
- * 
+ * @author Kent Madsen
+ * @author Goal Pioneers
  */
 public class TestingKeyStoreElement 
         implements KeyStoreElement
